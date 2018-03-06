1a3979a10b6df726cc67a3d2f5f946f994943410
Pouya <it.pouya@gmail.com>
2018-03-06 10:51:59 +0330

    rm

--- Sender2/__init__.py
+++
@@ -1,9 +0,0 @@
-import commands
-
-z= commands.getstatusoutput('nmap 127.0.0.1')
-a= commands.getoutput('nmap 127.0.0.1')
-#b= commands.getstatus('nmap 127.0.0.1')
-
-#print z
-print '='*40
-print a
--- Sender2/tcpSplitHandshake.py
+++
@@ -1,7 +0,0 @@
-import subprocess   
-cmd="ruby fakestack.rb"                                                                          
-p=subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)                              
-output, errors = p.communicate()
-print 'OUTPUT='+output
-print '_'*50
-print 'Errors='+str(errors)
